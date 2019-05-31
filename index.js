a07a6580db043519a1789765051bb7c6de258961
Jake <jakelovitz@gmail.com>
2019-05-31 10:46:48 -0400

    finished Control Flow Lab.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    let response = "This one is on me!";
+    return response;
+  } 
+  else if (feet > 2000) {
+    if (feet < 2500) {
+      let response = "I will gladly take your thirty bucks."
+      return response; }
+    else if (feet > 2500) {
+      let response  = "No can do."
+      return response;}
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.'
+      break;
+  }
 }
