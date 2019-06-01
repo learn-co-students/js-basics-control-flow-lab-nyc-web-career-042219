b1b759be3184908ab25e4db8fec703cdeb326596
henryk1229 <henryk1229@gmail.com>
2019-06-01 15:43:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet < 400) { return 'This one is on me!'}
+  else if (feet > 2000 && feet < 2500 ) { return 'I will gladly take your thirty bucks.'} else if (feet > 2500) { return 'No can do.'};
+};
+
+function ternaryCheckCity(city){
   // Write your code here!
+   return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+
+    default:
+      return 'Bye.';
+    };
 }
