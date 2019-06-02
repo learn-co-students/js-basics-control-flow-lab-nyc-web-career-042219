319ba40c9a413444a766841194ff67940c195660
Yasmine <y.hartung@gmail.com>
2019-06-02 10:32:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(length){
+  if(length < 400) {
+    return 'This one is on me!'
+  }else if (length >2000 && length < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
+
+}
+
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  if(tip === 'generous'){
+    return 'Thank you so much.'
+  } else if(tip === 'not as generous'){
+    return 'Thank you.'
+  } else {
+    return 'Bye.'
+  }
   // Write your code here!
 }
