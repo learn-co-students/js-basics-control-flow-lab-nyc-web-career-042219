e8f04b460259291352721612c61732092d1a91e7
Jason Gomez <jason.gomez@me.com>
2019-06-02 00:02:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
+  let res;
+  if (distance < 400) {
+    res = 'This is on me!';
+  } else if (distance > 2000 && distance <= 2500) {
+    res = 'I will gladly take your thirty bucks.';
+  } else {
+    res = 'No can do.';
+  }
+  return res;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let res;
+  switch (tip) {
+    case 'generous':
+      res = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      res = 'Thank you.';
+      break;
+    default:
+      res = 'Bye.';
+      break;
+  }
+  return res;
 }
